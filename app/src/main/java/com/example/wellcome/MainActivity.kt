654cf38d49ce35c8a96eec32756413ed5a0b3a7f
Jiale <sun952775@gmail.com>
2021-09-20 22:17:16 +0200

    Ajouter un bouton pour aller au page adduser

--- app/src/main/java/com/example/wellcome/MainActivity.kt
+++ app/src/main/java/com/example/wellcome/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.wellcome
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var btnadduser: Button = findViewById(R.id.adduser)
+        btnadduser.setOnClickListener(){
+            var intent = Intent(this,AddUserActivity::class.java)
+            startActivity(intent)
+        }
     }
+
 }
