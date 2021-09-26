65842880108088f1c1f0a0d495dab9dca13efbea
RayhaneJ <jebbarirayhane@gmail.com>
2021-09-26 16:08:05 +0200

    Menu

---
+++ app/src/main/java/com/example/wellcome/Menu.kt
@@ -0,0 +1,25 @@
+package com.example.wellcome
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.wellcome.databinding.ActivityMenuBinding
+
+class Menu : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMenuBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMenuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+}
