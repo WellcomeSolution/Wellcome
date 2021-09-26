38015214ba479c0318d6d171765f712985fd1aaf
RayhaneJ <jebbarirayhane@gmail.com>
2021-09-26 15:58:20 +0200

    Minor changes + Menu

--- app/src/main/java/com/example/wellcome/AddUserActivity.kt
+++ app/src/main/java/com/example/wellcome/AddUserActivity.kt
@@ -6,6 +6,7 @@ import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import com.example.wellcome.model.User
+
 //import base de donne
 class AddUserActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
