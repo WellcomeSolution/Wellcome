58021e211cda330fc08887c32fdb5f8ba511ad1f
RayhaneJ <jebbarirayhane@gmail.com>
2021-10-09 15:40:22 +0200

    Gradle Settings

---
+++ app/src/main/java/com/example/wellcome/Main2Activity.kt
@@ -0,0 +1,12 @@
+package com
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.wellcome.R
+
+class Main2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
