fc1a45870a59a58d24203ecbc8c36babb94929ae
rayhaneJ <rji8@3ds.com>
2021-10-11 21:24:12 +0200

    Unit test + Business model

--- wellcomelibrary/src/main/java/com/example/wellcome/models/Host.kt
+++ wellcomelibrary/src/main/java/com/example/wellcome/models/Host.kt
@@ -1,8 +1,7 @@
 package com.example.wellcome.models
 
-class Host(title: String, description: String, address: String, phone: String, tags: List<String>, val nombrePersonne: String, val nombrePiece:String) : Service(title, description,
+class Hosts(title: String, description: String, address: String, phone: String, tags: List<String>, val nombrePersonne: String, val nombrePiece:String) : Service(title, description,
     address,phone, tags)
 {
-
 
 }
