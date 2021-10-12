c52afcbe38e81e0bca001ece98b1faad7b592b5d
RayhaneJ <jebbarirayhane@gmail.com>
2021-10-13 00:11:41 +0200

    Add call action

--- app/src/main/java/com/example/wellcome/AssistanceAdapter.kt
+++ app/src/main/java/com/example/wellcome/AssistanceAdapter.kt
@@ -1,4 +1,5 @@
 package com.example.wellcome
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -6,19 +7,33 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.wellcome.R
 import com.example.wellcome.models.Assistance
+import com.example.wellcome.utils.call
+
 class AssistanceAdapter(private val dataSet: List<Assistance>):
     RecyclerView.Adapter<AssistanceAdapter.ViewHolder>()
 {
+    private lateinit var context: Context
+
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val title: TextView = itemView.findViewById(R.id.title_assistance)
         val address: TextView = itemView.findViewById(R.id.address_assistance)
         val phone: TextView = itemView.findViewById(R.id.phone_assistance)
         val tags: TextView = itemView.findViewById(R.id.tags_assistance)
         val priority: TextView = itemView.findViewById(R.id.priority_assistance)
+
+        init {
+            val callButton : TextView =  itemView.findViewById(R.id.call_button_assistance)
+            callButton.setOnClickListener{
+                context.call(Integer.parseInt(phone.text.toString()))
+            }
+        }
     }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AssistanceAdapter.ViewHolder {
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.assistance_card_view, parent, false)
+
+        context = parent.context
+
         return ViewHolder(view)
     }
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
--- app/src/main/java/com/example/wellcome/CoursAdapter.kt
+++ app/src/main/java/com/example/wellcome/CoursAdapter.kt
@@ -1,4 +1,5 @@
 package com.example.wellcome
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -6,20 +7,33 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.wellcome.R
 import com.example.wellcome.models.Lesson
+import com.example.wellcome.utils.call
 
 class CoursAdapter(private val dataSet: List<Lesson>):
     RecyclerView.Adapter<CoursAdapter.ViewHolder>()
 {
+    private lateinit var context:Context
+
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val title: TextView = itemView.findViewById(R.id.title_cours)
         val address: TextView = itemView.findViewById(R.id.address_cours)
         val phone: TextView = itemView.findViewById(R.id.phone_cours)
         val tags: TextView = itemView.findViewById(R.id.tags_cours)
         val sessionDuration: TextView = itemView.findViewById(R.id.sessionDuration_cours)
+
+        init {
+            val callButton : TextView =  itemView.findViewById(R.id.call_button_cours)
+            callButton.setOnClickListener{
+                context.call(Integer.parseInt(phone.text.toString()))
+            }
+        }
     }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CoursAdapter.ViewHolder {
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.cours_card_view, parent, false)
+
+        context = parent.context
+
         return ViewHolder(view)
     }
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
@@ -31,5 +45,6 @@ class CoursAdapter(private val dataSet: List<Lesson>):
     }
 
     override fun getItemCount() = dataSet.size
+
 
 }
