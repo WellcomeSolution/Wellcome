43a54f838ca5383da9041fb55f9aaecd3e4b4e76
RayhaneJ <jebbarirayhane@gmail.com>
2021-10-31 21:05:51 +0100

    [Refacto]

--- app/src/main/java/com/example/wellcome/ActivityConsultLesson.kt
+++ app/src/main/java/com/example/wellcome/ActivityConsultLesson.kt
@@ -3,19 +3,26 @@ package com.example.wellcome
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.room.Room
 import java.util.*
-import com.example.wellcome.utils.WellcomeDbContext
+import com.example.wellcome.utils.db.AppDatabase
 import kotlinx.android.synthetic.main.activity_consult_lesson.*
 
 
 class activity_consult_lesson: AppCompatActivity() {
-    val dbContext: WellcomeDbContext = WellcomeDbContext(this@activity_consult_lesson)
+    val db = Room.databaseBuilder(
+        this,
+        AppDatabase::class.java, "wellcome"
+    ).fallbackToDestructiveMigration().build()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(com.example.wellcome.R.layout.activity_consult_lesson)
-       val bundle = intent.extras
+        setContentView(R.layout.activity_consult_lesson)
+
+        val bundle = intent.extras
         val phoneNumber = bundle?.getString("phone")
         val pn : String = phoneNumber.toString()
+
         recycler_view_page.apply {
             layoutManager= LinearLayoutManager(this@activity_consult_lesson)
             adapter=ConsultLessonAdapter(this@activity_consult_lesson,dbContext.searchLessonByPhone(pn))
