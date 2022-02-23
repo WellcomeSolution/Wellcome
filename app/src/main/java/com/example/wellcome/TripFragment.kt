985b3c46953603d0286144a916c7d2cf1d1b53b6
Jiale <sun952775@gmail.com>
2022-02-24 00:17:44 +0100

    ajouter snackbar pour search button

--- app/src/main/java/com/example/wellcome/TripFragment.kt
+++ app/src/main/java/com/example/wellcome/TripFragment.kt
@@ -11,6 +11,7 @@ import com.example.wellcome.data.SharedTripViewModel
 import com.example.wellcome.databinding.FragmentTripBinding
 import kotlinx.android.synthetic.main.fragment_trip.*
 import android.R
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.navigation.Navigation
 import com.example.services.TripPattern
@@ -18,6 +19,7 @@ import com.example.services.TripPattern
 import androidx.lifecycle.Lifecycle
 import com.example.services.HostPresenter
 import com.example.wellcome.data.UserViewModel
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.transition.MaterialSharedAxis
 
 
@@ -62,16 +64,41 @@ class TripFragment : Fragment() {
         }
 
         search_button.setOnClickListener{
-            val pattern = TripPattern(
-                userViewModel.email.value!!,
-                viewModel.adults.value!!,
-                viewModel.babies.value!!,
-                viewModel.childs.value!!,
-                viewModel.city.value?.lon!!.toDouble(),
-                viewModel.city.value?.lat!!.toDouble()
-            )
-            viewModel.loadHosts(pattern)
+            var adults =  viewModel.adults.value!!
+            var babies = viewModel.babies.value!!
+            var childs = viewModel.childs.value!!
+            var lon  = viewModel.city.value?.lon!!
+            if((adults == 0 && babies == 0 && childs == 0)||adults == null || babies == null || childs == null){
+                Snackbar.make(search_button,"Please select travelers!",Snackbar.LENGTH_LONG)
+                    .show()
+            }
+            else if(lon.equals(null) || lon == ""){
+                Snackbar.make(search_button,"Please select your location!",Snackbar.LENGTH_LONG)
+                    /*message dans R.id.string pour dire que search fonctionne pas */
+                /*  .setAction("undo"){
+                   Toast.makeText(context,"Please select your location!",Toast.LENGTH_SHORT)
+                il faut une fonction de initialiser les 3 champs
+                    }*/
+                    .show()
+            }
+            /*else if((adults == 0 && babies == 0 && childs == 0)||adults == null || babies == null || childs == null){
+                Snackbar.make(search_button,"Please select travelers!",Snackbar.LENGTH_LONG)
+                    .show()
+            }*/
+            else{
+                val pattern = TripPattern(
+                    userViewModel.email.value!!,
+                    viewModel.adults.value!!,
+                    viewModel.babies.value!!,
+                    viewModel.childs.value!!,
+                    viewModel.city.value?.lon!!.toDouble(),
+                    viewModel.city.value?.lat!!.toDouble()
+                )
+                viewModel.loadHosts(pattern)
+            }
+
         }
+
     }
 
     override fun onCreateView(
